f91a4df7fcaa88e205f185fdfeeebf1111a8fac2
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-08-16 22:42:27 +0100

    added hero section

---
+++ components/Hero.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+	return (
+		<section className="w-full flex-center flex-col">
+			<h1 className="head_text text-center">Discover and Create</h1>
+			<br className="max-md:hidden" />
+			<span className="blue_text blue_gradient text-center">
+				Programming Tips
+			</span>
+
+			<p className="desc text-center pb-5">
+				Get programming tips from the best developers around the world.
+			</p>
+		</section>
+	);
+};
+
+export default Home;
