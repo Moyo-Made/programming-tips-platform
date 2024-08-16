1ce6b0da7972f1998b4762d78de6f13066dc159d
Moyo-Made <adegbitemoyomade2004@gmail.com>
2024-08-16 22:44:53 +0100

    added tip list

--- components/TipList.jsx
+++ components/TipList.jsx
@@ -1,13 +1,12 @@
-import TipCard from './TipCard';
+import TipCard from "./TipCard";
 import { tips } from "@/lib/mockData";
 
 export default function TipList() {
-
-  return (
-    <div className='flex flex-wrap gap-10 '>
-      {tips.map((tip) => (
-        <TipCard key={tip.id} tip={tip} />
-      ))}
-    </div>
-  );
+	return (
+		<div className="flex flex-wrap justify-center gap-5 px-10 mt-5">
+			{tips.map((tip) => (
+				<TipCard key={tip.id} tip={tip} />
+			))}
+		</div>
+	);
 }
